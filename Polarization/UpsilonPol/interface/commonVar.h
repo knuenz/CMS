23f4d58ced4adb38452c9d3d797588cbfa29ff25
knuenz <>
2012-04-16 06:45:46 +0000

    Developments

--- Polarization/UpsilonPol/interface/commonVar.h
+++ Polarization/UpsilonPol/interface/commonVar.h
@@ -19,17 +19,36 @@ namespace onia{
   Double_t rapRange[2*kNbRapBins+1] = {-1.2, -0.6, -0., 0.6, 1.2};
 //  Double_t rapRange[2*kNbRapBins+1] = {-0.6, -0.3, -0., 0.3, 0.6};
 
-  Int_t const kNbPTMaxBins = 14;
-  Int_t const kNbPTBins[kNbRapForPTBins+1] = {kNbPTMaxBins, 12, 12};//all y, y1
+  Int_t const kNbPTMaxBins = 10;
+/*  Int_t const kNbPTBins[kNbRapForPTBins+1] = {kNbPTMaxBins, 12, 12};//all y, y1
   Double_t pTRange[kNbRapForPTBins+1][kNbPTMaxBins+1] = {
     {5., 6., 7., 8., 9., 10., 11., 12., 14., 16., 20., 25., 30., 50., 100.},//all rapidities
     {5., 6., 7., 8., 9., 10., 11., 12., 14., 16., 20., 30., 50.},//mid-rap
     {5., 6., 7., 8., 9., 10., 11., 12., 14., 16., 20., 30., 50.}};//most forward
-/*  Double_t pTRange[kNbRapForPTBins+1][kNbPTMaxBins+1] = {
-    {5., 6., 7., 8., 9., 10., 11., 12., 14., 16., 20., 25., 30., 50., 100.},//all rapidities
-    {10.,11.,12.,13., 14.,15.,16.,17.,18., 20.,24., 30., 50.},//mid-rap
-    {10.,11.,12.,13., 14.,15.,16.,17.,18., 20.,24., 30., 50.}};//most forward
-*/  //number of reference frames
+*/
+
+  Int_t const kNbPTBins[kNbRapForPTBins+1] = {kNbPTMaxBins, 10, 10};//all y, y1
+  Double_t pTRange[kNbRapForPTBins+1][kNbPTMaxBins+1] = {
+	{5., 6., 7., 8., 9., 10., 12., 16., 20., 30., 50.},//all rapidities
+    {5., 6., 7., 8., 9., 10., 12., 16., 20., 30., 50.},//mid-rap
+    {5., 6., 7., 8., 9., 10., 12., 16., 20., 30., 50.}};//most forward
+  // High ctau
+/*    Int_t const kNbPTBins[kNbRapForPTBins+1] = {3, 3, 3};//all y, y1
+  	  Double_t pTRange[kNbRapForPTBins+1][kNbPTMaxBins+1] = {
+  			  {10., 13., 18., 30.},//mid-rap
+  			  {10., 13., 18., 30.},//mid-rap
+  			  {10., 13., 18., 30.}};//mid-rap
+*/
+
+// SingleMuPD binning
+/*  Int_t const kNbPTBins[kNbRapForPTBins+1] = {2, 2, 2};//all y, y1
+	  Double_t pTRange[kNbRapForPTBins+1][kNbPTMaxBins+1] = {
+			  {20., 30., 50.},//all rapidities
+			  {20., 30., 50.},//mid-rap
+			  {20., 30., 50.}};//most forward
+*/
+
+  //number of reference frames
   Int_t const kNbFrames = 6;
   Char_t *frameLabel[kNbFrames] = {"CS", "HX", "PHX", "sGJ", "GJ1", "GJ2"};
   enum {CS, HX, PHX, sGJ, GJ1, GJ2};
@@ -37,10 +56,10 @@ namespace onia{
   Int_t const kNbPolVar = 2; //cosTheta, phi
   enum {cosThPol,phiPol};
   //cosTheta
-  Int_t const kNbBinsCosT = 10;
+  Int_t const kNbBinsCosT = 15;
   Double_t cosTMin = -1., cosTMax = 1.;
   //phi for pol. 
-   Int_t const kNbBinsPhiPol = 10;
+   Int_t const kNbBinsPhiPol = 15;
   Double_t phiPolMin = -180., phiPolMax = 180.;
   Double_t massMinL = 8.6; //left mass edge of the BG mass window
   Double_t massMaxR = 11.4; //right mass edge of the BG mass window
@@ -50,6 +69,9 @@ namespace onia{
   //phase space limiting cuts:
   Double_t rapYPS = 1.2;
   Double_t JpsiCtauMax = 1000.; //effectively no cut on lifetime
+  Double_t JpsiCtauMin = -1000; //effectively no cut on lifetime
+  Double_t JpsiCtauSigMin=-1000; //-1000
+  Double_t JpsiCtauSigMax=2.; //1000
   /* Double_t nSigMass = 1000.; */
   Double_t nSigMass = 2.;
   /* Double_t polMassJpsi[kNbRapForPTBins+1] = {3.094, 3.094, 3.094, 3.092, 3.092, 3.092};//[all rap, rap bin 1-5] */
@@ -59,8 +81,8 @@ namespace onia{
 
   //some make up to use the same colour and marker for each pT and rapidity bin
   //in every plotting macro:
-  Int_t colour_pT[kNbPTMaxBins+1] = {1, 2, 3, 4, 6, 7, 8, 49, 38, 46, 12, 40};
-  Int_t marker_pT[kNbPTMaxBins+1] = {20, 21, 25, 22, 23, 26, 27, 28, 29, 30, 20, 20};
+//  Int_t colour_pT[kNbPTMaxBins+1] = {1, 2, 3, 4, 6, 7, 8, 49, 38, 46, 12, 40};
+//  Int_t marker_pT[kNbPTMaxBins+1] = {20, 21, 25, 22, 23, 26, 27, 28, 29, 30, 20, 20};
   
   /* Int_t colour_rapForPTBins[kNbRapForPTBins+1] = {1, 30, 4, 2, 3, kMagenta+1}; */
   /* Int_t marker_rapForPTBins[kNbRapForPTBins+1] = {20, 21, 25, 20, 22, 29}; */
